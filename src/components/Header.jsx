e1be06eb54b785b31d4ba4fbe184c8e7dcaea764
HgaMen <hga.int2021@gmail.com>
2022-10-26 15:02:01 -0500

    dia 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import IconMoon from "./icons/IconMoon";
+import IconSun from "./icons/IconSun";
+
+const inicialStateDarkMode = localStorage.getItem("theme") === "dark";
+ 
+const Header = () => {
+    const [darkMode, setDarkMode] = useState(inicialStateDarkMode);
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            // localStorage.theme = "dark"
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            // localStorage.theme = "light"
+        }
+    }, [darkMode]);
+
+    return (
+        <header className="container mx-auto px-4 pt-8">
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-semibold uppercase tracking-[0.5rem] text-white">
+                    Todo
+                </h1>
+                <button onClick={() => setDarkMode(!darkMode)}>
+                    {darkMode ? <IconSun /> : <IconMoon />}
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
